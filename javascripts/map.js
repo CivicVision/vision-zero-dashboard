8dd2b7f3f4db20667b0f4b6788ddcdd7c0c982bb
milafrerichs <mila.frerichs@gmail.com>
2017-07-17 13:23:58 +0200

    Add map with all traffic fatalities this year
    geocode them via google maps api and add an info window

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,3 +1,4 @@
+var geocoder;
 function formatAddress(d) {
 	street_name = d.street_name+' '+d.street_type;
 	if(d.cross_st_name) {
@@ -16,6 +17,7 @@ function initMap(lastAccident) {
 		center: {lat: 32.824, lng: -117.235},
     disableDefaultUI: true
 	});
+  geocoder = new google.maps.Geocoder();
   var sv = new google.maps.StreetViewService();
   panorama = new google.maps.StreetViewPanorama(
     document.getElementById('map-pano'),
@@ -24,7 +26,6 @@ function initMap(lastAccident) {
       panControl: false,
       enableCloseButton: false
     });
-	var geocoder = new google.maps.Geocoder();
   address = formatAddress(lastAccident);
 	geocoder.geocode({'address': address}, function(results, status) {
 		if (status === 'OK') {
@@ -46,4 +47,39 @@ function initMap(lastAccident) {
       document.getElementById('map-error').innerHTML = 'Unable to find the address to show on a map';
 		}
 	});
+}
+function addAccidentToMap(map, accident) {
+  var address = formatAddress(accident);
+  var infowindow = new google.maps.InfoWindow({
+  });
+  function addToMap(results, status) {
+    var dateFormat = d3.timeFormat('%B, %d');
+    var timeFormat = d3.timeFormat('%I %p');
+    var date = dateFormat(new Date(this.date_time))+' '+timeFormat(new Date(this.date_hour));
+    var address = formatAddress(this);
+    var contentString = '<h5>'+address+'</h5><p><b>Date:</b> '+date+'</p><p><b># fatalities:</b> '+this.killed+'<br/><b># injuries:</b> '+this.injured+'</p>';
+    if (status === 'OK') {
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location,
+        title: contentString,
+        label: this.killed
+      });
+      google.maps.event.addListener(marker,'click', function() {
+        infowindow.open(map, this);
+        infowindow.setContent(this.title);
+      });
+    } else { }
+  }
+  geocoder.geocode({'address': address}, addToMap.bind(accident));
+}
+function show2017Map(accidents) {
+	var map2017 = new google.maps.Map(document.getElementById('map-2017'), {
+		zoom: 10,
+		center: {lat: 32.824, lng: -117.235}
+	});
+  var accidentsLength = accidents.length;
+  for(var i=0;i<accidentsLength;i++) {
+    addAccidentToMap(map2017, accidents[i]);
+  }
 }

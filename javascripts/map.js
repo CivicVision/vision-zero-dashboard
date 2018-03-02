b51f3427031c939b6e28071563d68087938cde36
milafrerichs <mila.frerichs@gmail.com>
2018-03-02 21:09:17 +0100

    Add a heatmap for the accidents

--- javascripts/map.js
+++ javascripts/map.js
@@ -66,12 +66,43 @@ function addAccidentToMap(map, accident) {
   });
 }
 function show2017Map(accidents) {
+  var silverStyle = [ { elementType: 'geometry', stylers: [{color: '#f5f5f5'}] },
+            { elementType: 'labels.icon', stylers: [{visibility: 'off'}] },
+            { elementType: 'labels.text.fill', stylers: [{color: '#616161'}] },
+            { elementType: 'labels.text.stroke', stylers: [{color: '#f5f5f5'}] },
+            { featureType: 'administrative.land_parcel', elementType: 'labels.text.fill', stylers: [{color: '#bdbdbd'}] },
+            { featureType: 'poi', elementType: 'geometry', stylers: [{color: '#eeeeee'}] },
+            { featureType: 'poi', elementType: 'labels.text.fill', stylers: [{color: '#757575'}] },
+            { featureType: 'poi.park', elementType: 'geometry', stylers: [{color: '#e5e5e5'}] },
+            { featureType: 'poi.park', elementType: 'labels.text.fill', stylers: [{color: '#9e9e9e'}] },
+            { featureType: 'road', elementType: 'geometry', stylers: [{color: '#ffffff'}] },
+            { featureType: 'road.arterial', elementType: 'labels.text.fill', stylers: [{color: '#757575'}] },
+            { featureType: 'road.highway', elementType: 'geometry', stylers: [{color: '#dadada'}] },
+            { featureType: 'road.highway', elementType: 'labels.text.fill', stylers: [{color: '#616161'}] },
+            { featureType: 'road.local', elementType: 'labels.text.fill', stylers: [{color: '#9e9e9e'}] },
+            { featureType: 'transit.line', elementType: 'geometry', stylers: [{color: '#e5e5e5'}] },
+            { featureType: 'transit.station', elementType: 'geometry', stylers: [{color: '#eeeeee'}] },
+            { featureType: 'water', elementType: 'geometry', stylers: [{color: '#c9c9c9'}] },
+            { featureType: 'water', elementType: 'labels.text.fill', stylers: [{color: '#9e9e9e'}] }
+          ];
+  var styledMapType = new google.maps.StyledMapType(silverStyle,{name: 'Styled Map'});
 	var map2017 = new google.maps.Map(document.getElementById('map-2018'), {
 		zoom: 10,
 		center: {lat: 32.824, lng: -117.235}
 	});
+  map2017.mapTypes.set('styled_map', styledMapType);
+  map2017.setMapTypeId('styled_map');
   var accidentsLength = accidents.length;
-  for(var i=0;i<accidentsLength;i++) {
-    addAccidentToMap(map2017, accidents[i]);
+  var heatmapData = [];
+  for (var i = 0; i < accidentsLength; i++) {
+    var accidentLocation = latLngFromAccident(accidents[i]);
+    if (!isNaN(accidentLocation.lat) && !isNaN(accidentLocation.lng)) {
+      var latLng = new google.maps.LatLng(accidentLocation);
+      heatmapData.push(latLng);
+    }
   }
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data: heatmapData,
+    map: map2017
+  });
 }
